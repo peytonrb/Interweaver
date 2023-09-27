d6d05be19e03f67159e6fe3a1c588197028d7960
treemanrocket <56743498+treemanrocket@users.noreply.github.com>
2023-09-27 11:34:37 -0400

    weaveable floats
    weaveable object now floats

--- Assets/Scripts/WeaveMechanics/Weaveable.cs
+++ Assets/Scripts/WeaveMechanics/Weaveable.cs
@@ -3,14 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Weaveable : MonoBehaviour, IInteractable
+
 {
+
+    [Header("Weaveable's variables")]
+    [SerializeField] private Rigidbody rigidbody;
+    [SerializeField] private float HoveringValue;
+    private bool Startfloating;
+    
+
+    void start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Startfloating) 
+        {
+           transform.position = transform.position + new Vector3 (0, HoveringValue*Time.deltaTime, 0);
+            Startfloating = false;
+        }     
+    }
+
     public void Interact()
     {
         Debug.Log("This is interactable");
+        rigidbody.useGravity = false;
+        Startfloating = true;
+       
     }
-
+    
     public void Uninteract()
     {
-        Debug.Log("this is now not  woven");
+        Debug.Log("this is now not woven");
+        rigidbody.useGravity = true;
+        
     }
 }
