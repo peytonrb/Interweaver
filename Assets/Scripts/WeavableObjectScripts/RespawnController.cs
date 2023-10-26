ad021085a6e50da4f1eff3629db739f97ffdd517
Peyton Bischof <peyton.bischof@hotmail.com>
2023-10-25 23:46:06 -0400

    Weavable Object Respawn

---
+++ Assets/Scripts/WeavableObjectScripts/RespawnController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnController : MonoBehaviour
+{
+    [SerializeField] private Vector3 startPosition;
+    [SerializeField] private Quaternion startRotation;
+    [SerializeField] private GameObject respawnObject;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        startPosition = collider.gameObject.GetComponent<RespawnableObject>().spawnPos;
+        startRotation = collider.gameObject.GetComponent<RespawnableObject>().spawnRotation;
+        respawnObject = collider.gameObject;
+
+        RespawnObject();
+    }
+
+    // can be called in puzzles and other events that require respawning objects
+    public void RespawnObject()
+    {
+        // if respawn isn't caused by a collision
+        if (startPosition == null || startRotation == null || respawnObject == null)
+        {
+            InitializeObject();
+        }
+
+        respawnObject.transform.position = startPosition;
+        respawnObject.transform.rotation = startRotation;
+    }
+
+    private void InitializeObject()
+    {
+        // populate once this case occurs, unsure how these will get initialized rn
+    }
+}
