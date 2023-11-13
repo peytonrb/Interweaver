e169dfe2a04452250ade1971b656db07e832c105
Jack Murray <murray.j909@gmail.com>
2023-11-12 23:21:58 -0500

    AHHHHHHHHHHHHH

--- Assets/Scripts/WeaveMechanics/Crystals/HoverCrystalScript.cs
+++ Assets/Scripts/WeaveMechanics/Crystals/HoverCrystalScript.cs
@@ -1,9 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class HoverCrystalScript : MonoBehaviour
 {
