f381caeea97c881fb042fbabb11296628202be53
treemanrocket <56743498+treemanrocket@users.noreply.github.com>
2024-01-18 10:25:56 -0500

    funny comments in the script and slight changes
    slight changes for the script.

--- Assets/Scripts/DarknessMechanicScript/LightSourceScript.cs
+++ Assets/Scripts/DarknessMechanicScript/LightSourceScript.cs
@@ -25,31 +25,32 @@ public class LightSourceScript : MonoBehaviour
     }
     void Update()
     {
-        BatchRaycastForLights();
+        BatchRaycastForLights(); 
     }
 
-    void BatchRaycastForLights()
+    void BatchRaycastForLights() //will need to make this better so that it doesn't happen every frame, kind of cringe ngl
     {
         
         foreach (LightData lightData in lightsArray)
         {
-            Light lightSource = lightData.lightSource;
+            Light lightSource = lightData.lightSource; //this is from the public struct
 
-            float maxDistance = lightData.maxDistance;
+            float maxDistance = lightData.maxDistance; //also from the public struct
             
             lightSource.range = maxDistance;
 
-            lightData.lightCollider.transform.localScale = new Vector3 (maxDistance,0,0);
+            lightData.lightCollider.transform.localScale = new Vector3 (maxDistance,0,0); //clickty clackty, this is from the public structy
 
             Vector3 directionToPlayer = playerTransform.position - lightSource.transform.position;
-          
 
-            RaycastHit[] hits = Physics.RaycastAll(lightSource.transform.position, directionToPlayer, maxDistance, obstructionView);
+            //the batch raycast from all light sources in the array will point towards the player and will do somehing if the object between the player and the  light source if it has the layer
+            RaycastHit[] hits = Physics.RaycastAll(lightSource.transform.position, directionToPlayer, maxDistance, obstructionView); 
 
-            Debug.DrawRay(lightSource.transform.position, directionToPlayer * maxDistance /2, Color.green);
+            Debug.DrawRay(lightSource.transform.position, directionToPlayer * maxDistance /2f, Color.green);
 
             foreach (RaycastHit hit in hits)
-            {        
+            {   
+                //inside this logic this can be used for obstructing the view if there is an object with the obstruction layer
                 Debug.Log("Light source: " + lightSource.name + " - Obstruction by: " + hit.collider.gameObject.name);
             }
         }
