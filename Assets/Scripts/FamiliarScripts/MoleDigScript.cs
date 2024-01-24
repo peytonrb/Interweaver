8494ed676a0cd201607be78d95d167a9b740f2f0
treemanrocket <56743498+treemanrocket@users.noreply.github.com>
2024-01-23 19:02:57 -0500

    progress for the mole digging
    progress for the mole digging stuff

--- Assets/Scripts/FamiliarScripts/MoleDigScript.cs
+++ Assets/Scripts/FamiliarScripts/MoleDigScript.cs
@@ -8,48 +8,44 @@ public class MoleDigScript : MonoBehaviour
     public LayerMask digableLyer;
     public float castDistance;
     public bool digThroughGround;
+    
     void Start()
     {
         digThroughGround = false;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         Debug.DrawRay(transform.position, -transform.up, Color.red);
-
-        if (OnDigableLayer())
-        {          
-            Debug.Log("yooo this is digable?");
-        }
     }
 
-    public bool OnDigableLayer()
-    {
-        if (Physics.Raycast(transform.position, -transform.up, castDistance, digableLyer))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
+  
 
     public void DigPressed()
     {
-        if (!digThroughGround && OnDigableLayer())
+        RaycastHit hitLayer;
+
+        if (Physics.Raycast(transform.position, -transform.up, out hitLayer, castDistance, digableLyer) && !digThroughGround)
         {
             Debug.Log("we're digging bois");
             digThroughGround = true;
+            DigAction(hitLayer.collider);
+            //Debug.Log(hitLayer.collider.gameObject.name);
         }
-
-        else if (digThroughGround && OnDigableLayer())
+             
+        else if (Physics.Raycast(transform.position, -transform.up, out hitLayer, castDistance, digableLyer) && digThroughGround)
         {
             Debug.Log("we got out bois");
             digThroughGround = false;
         }
+
     }
 
-
+    public void DigAction(Collider funnyBox)
+    {        
+        //Debug.Log("we actually digging this bois " + funnyBox.gameObject.name);
+        Debug.Log("the bounds of the box " + funnyBox.bounds);
+    }
 }
