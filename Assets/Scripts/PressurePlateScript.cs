ea8d1b1f39cf2ffaff7d91fa5a9000c874679847
Saving Progress <97535452+SaveScreen@users.noreply.github.com>
2024-01-26 08:06:48 -0500

    Finally finished that darn pressure plate

--- Assets/Scripts/PressurePlateScript.cs
+++ Assets/Scripts/PressurePlateScript.cs
@@ -4,20 +4,74 @@ using UnityEngine;
 
 public class PressurePlateScript : MonoBehaviour
 {
-    /*
-    void OnControllerColliderHit(ControllerColliderHit hit) {
-        if (hit.gameObject.CompareTag("Player")) {
-            Debug.Log("Yes");
-        }
-       
+    private bool standingOnPlate; //If player is standing on plate, this is true
+    private bool bottomedOut; //If its bottomed out, stop moving it
+    private bool toppedOut; //Same thing as bottomed out except in reverse
+    public float weight; //Player and familiar will have the same weight
+    private bool activated;
+    private float bottomtargetposition;
+    private float toptargetposition;
+
+    void Start() {
+        bottomtargetposition = 0.1f;
+        toptargetposition = 0.3f;
+        bottomedOut = false;
+        toppedOut = true;
+        activated = false;
     }
-    */
+
+    void Update() {
+        if (standingOnPlate && !bottomedOut) {
+            PushPlate();
+        }
+        else if (!standingOnPlate && !toppedOut) {
+            PullPlate();
+        }
+        else if (activated) {
+            Activation();
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Familiar"))
         {
-            Debug.Log("Yes");
+            standingOnPlate = true;
         }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Familiar")) 
+        {
+            standingOnPlate = false;
+        }
+    }
+
+    void PushPlate() {
+        if (transform.position.y > bottomtargetposition) {
+            float newposition = Mathf.MoveTowards(transform.position.y, bottomtargetposition, weight * Time.deltaTime);
+            transform.position = new Vector3(transform.position.x, newposition, transform.position.z);
+        }
+        else {
+            bottomedOut = true;
+            activated = true;
+        }
+        toppedOut = false;
+    }
+
+    void PullPlate() {
+        if (transform.position.y < toptargetposition) {
+            float newposition = Mathf.MoveTowards(transform.position.y, toptargetposition, weight * Time.deltaTime);
+            transform.position = new Vector3(transform.position.x, newposition, transform.position.z);
+        }
+        else {
+            toppedOut = true;
+        }
+        bottomedOut = false;
+        activated = false;
+    }
+
+    void Activation() {
+        Debug.Log("Do whatever you want");
     }
 }
