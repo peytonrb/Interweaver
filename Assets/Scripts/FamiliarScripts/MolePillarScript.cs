b1e7731dee094103a405e35df5e5029e9ff911a8
CarolineObvi <carolinejmorian@gmail.com>
2024-01-27 20:06:44 -0500

    pillar progress yahoocd Interweaver/cd Interweaver/

--- Assets/Scripts/FamiliarScripts/MolePillarScript.cs
+++ Assets/Scripts/FamiliarScripts/MolePillarScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class MolePillarScript : MonoBehaviour
@@ -9,10 +10,11 @@ public class MolePillarScript : MonoBehaviour
     [SerializeField] private GameObject dirtPillar;
     public List<GameObject> pillarList = new List<GameObject>();
     [Header("Variables")]
-    [SerializeField] private int maxPillarCount = 1;
+    [SerializeField] [Range (1, 10)] private int maxPillarCount = 1;
     [SerializeField] [Range (10f, 50)] private float maxPillarHeight = 25f;
     [SerializeField] private float pillarBuildSpeed = 1.5f;
-    private bool pillarBuildBegan;
+    private bool pillarBuilding;
+    private Vector3 pointToRiseTo = Vector3.up;
     private float distance = -1f;
     public bool deploy;
     public bool scronk;
@@ -39,6 +41,12 @@ public class MolePillarScript : MonoBehaviour
 
     public void DeployPillar()
     {
+        if (pillarList.Count() >= maxPillarCount)
+        {
+            Destroy(pillarList[0]);
+            pillarList.RemoveAt(0);
+        }
+
         GameObject newPillar = Instantiate(dirtPillar);
         newPillar.transform.position = transform.position;
         pillarList.Add(newPillar);
@@ -51,18 +59,32 @@ public class MolePillarScript : MonoBehaviour
 
     public void RaisePillar()
     {
-        pillarList[maxPillarCount - 1].transform.position = Vector3.MoveTowards(pillarList[0].transform.position, Vector3.up * 50f, pillarBuildSpeed * Time.deltaTime);
+        distance = Vector3.Distance(pillarList[pillarList.Count-1].transform.position, pointToRiseTo);
+        if (!pillarBuilding)
+        {
+            pointToRiseTo = transform.position + (Vector3.up * maxPillarHeight);
+            pillarBuilding = true;
+        }
+        else if (distance > 0.1f)
+        {
+            pillarList[pillarList.Count-1].transform.position = Vector3.MoveTowards(pillarList[pillarList.Count-1].transform.position, pointToRiseTo, pillarBuildSpeed * Time.deltaTime);
+        }
+        else
+        {
+            scronk = false; // auto stop input
+            pillarBuilding = false;
+        }
     }
 
     void OnDrawGizmos()
     {
-        /*if (!pillarBuildBegan)
+        if (!pillarBuilding)
         {
             DrawArrow.ForGizmo(transform.position, Vector3.up * maxPillarHeight);
         }
         else
         {
             DrawArrow.ForGizmo(transform.position, Vector3.up * distance);
-        }*/
+        }
     }
 }
