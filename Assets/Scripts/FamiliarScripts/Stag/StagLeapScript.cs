42503b525286efe598bb01b34f7fd085d7237986
CarolineObvi <carolinejmorian@gmail.com>
2024-02-11 19:07:16 -0500

    stag leap & slam

--- Assets/Scripts/FamiliarScripts/Stag/StagLeapScript.cs
+++ Assets/Scripts/FamiliarScripts/Stag/StagLeapScript.cs
@@ -11,40 +11,52 @@ public class StagLeapScript : MonoBehaviour
     private MovementScript movementScript;
     private CharacterController characterController;
     [Header("Variables")]
-    private float currentJumpForce = 0f; 
-    private float t = 0f;
-    [HideInInspector] public bool chargingJump;
+    private float currentJumpForce = 0f; // the current jump force
+    //private float t = 0f;
+    [HideInInspector] public bool chargingJump; // is a jump currently being charged?
     [Header("Leap Variables")]
-    [SerializeField] private float jumpChargeTime = 3f;
-    [SerializeField] [Range (1f, 50f)] private float maxJumpForce = 10f;
+    [SerializeField] private float jumpChargeTime = 3f; // how long is takes to charge a jump to max
+    private Vector3 groundLocation; // marks the position of the ground directly below the stag while airborne
+    private bool displayGroundGizmos; // displays gizmo 
+    [SerializeField] [Range (1f, 75f)] private float maxJumpForce = 10f;
     [Header("Slam Variables")]
-    [SerializeField] private float inititalSlamForce = -10f;
-    [SerializeField] private float slamGravity = -30;
-    private float startingHeight;
-    private bool canSlam;
-    private bool displayGizmos;
+    [SerializeField] private float inititalSlamForce = -10f; // the initial slam force, if y velocity is already lower, just adds this number
+    [SerializeField] private float slamGravity = -30; // gravity of slam
+    private float startingHeight; // starting height of slam
+    [SerializeField] private bool canOnlySlamAfterLeap; // determines if slams can only be done after a leap
+    private bool canSlam; // can the stag slam at this moment?
+
+    private bool displaySlamGizmos; // displays size of slam
+    [SerializeField][Range (2f, 6f)] private float maxSlamRadius = 5f;
+    [SerializeField] private float slamTime = 0.6f;
+    private float currentSlamRadius = 0f;
+    [SerializeField][Range(0f, 3f)] private float postSlamStaggerTime = 0.6f;
     // Start is called before the first frame update
     void Start()
     {
         movementScript = GetComponent<MovementScript>();
         characterController = GetComponent<CharacterController>();
+        if (!canOnlySlamAfterLeap)
+        {
+            canSlam = true;
+        }
     }
 
     public IEnumerator ChargeJump()
     {
         float startTime = Time.time;
+        float t = 0f;
 
         if (characterController.isGrounded)
         {
             chargingJump = true;
+            movementScript.ZeroCurrentSpeed();
             movementScript.canMove = false;
         }
         else
         {
             if (canSlam)
             {
-                movementScript.ChangeVelocity(new Vector3(movementScript.GetVelocity().x, -inititalSlamForce, movementScript.GetVelocity().z));
-                startingHeight = transform.position.y;
                 StartCoroutine(StartSlam());
             }
         }
@@ -62,6 +74,7 @@ public class StagLeapScript : MonoBehaviour
     {
         if (chargingJump)
         {
+            StartCoroutine(ShowGroundMarker());
             canSlam = true;
             movementScript.canMove = true;
             chargingJump = false;
@@ -75,31 +88,84 @@ public class StagLeapScript : MonoBehaviour
     private IEnumerator StartSlam()
     {
         canSlam = false;
-        RaycastHit hit;
-        Physics.SphereCast(transform.position + characterController.center, 1f, Vector3.down, out hit);
-        if (hit.collider != null)
+
+        if (movementScript.GetVelocity().y >= 0)
         {
-            Debug.Log("EEEEEEEEEEEE");
+            movementScript.ChangeVelocity(new Vector3(movementScript.GetVelocity().x, inititalSlamForce, movementScript.GetVelocity().z));
         }
+        else
+        {
+            movementScript.ChangeVelocity(new Vector3(movementScript.GetVelocity().x, movementScript.GetVelocity().y + inititalSlamForce, movementScript.GetVelocity().z));
+        }
+        startingHeight = transform.position.y;
         
         while (!characterController.isGrounded)
         {
             yield return null;
         }
-        Debug.Log("Distance Fallen: " + (startingHeight - transform.position.y));
-        //RaycastHit hit;
-        displayGizmos = true;
-        //Physics.SphereCast(transform.position + characterController.center, 5f, Vector3.zero, out hit);
-        yield return new WaitForSeconds(1f);
-        displayGizmos = false;
+        // put all impact stuff past this point
+        //Debug.Log("Distance Fallen: " + (startingHeight - transform.position.y));
+        StartCoroutine(GrowSlamRadius());
+        canSlam = true;
+        movementScript.canMove = false;
+        yield return new WaitForSeconds(postSlamStaggerTime);
+        if (!chargingJump)
+        {
+            movementScript.canMove = true;
+        }
+    }
+
+    private IEnumerator ShowGroundMarker()
+    {
+        yield return new WaitForFixedUpdate();
+        RaycastHit hit;
+        while (!characterController.isGrounded)
+        {
+            Physics.SphereCast(transform.position + characterController.center, 1f, Vector3.down, out hit);
+            if (hit.collider != null)
+            {
+                displayGroundGizmos = true; // we'll replace this with some other kind of marker later on
+                groundLocation = hit.point;
+            }
+            yield return null;
+        }
+        displayGroundGizmos = false;
+    }
+
+    private IEnumerator GrowSlamRadius()
+    {
+        
+        displaySlamGizmos = true;
+        float startTime = Time.time;
+        float t = 0f;
+        while (Time.time < startTime + slamTime)
+        {
+            t += Time.deltaTime / slamTime;
+            currentSlamRadius = Mathf.Lerp(0, maxSlamRadius, t);
+            RaycastHit[] hits = Physics.SphereCastAll(transform.position + characterController.center, currentSlamRadius, Vector3.down);
+            foreach (RaycastHit hit in hits)
+            {
+                if (hit.collider.gameObject.CompareTag("Breakable"))
+                {
+                    Destroy(hit.collider.gameObject); // call the thing being broken here!
+                }
+            }
+            yield return null;
+        }
+        displaySlamGizmos = false;
     }
 
     private void OnDrawGizmos()
     {
-        if (displayGizmos)
+        if (displayGroundGizmos)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawSphere(groundLocation, 1f);
+        }
+        if (displaySlamGizmos)
         {
             Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(transform.position, 5f);
+            Gizmos.DrawSphere(transform.position, currentSlamRadius);
         }
     }
 }
