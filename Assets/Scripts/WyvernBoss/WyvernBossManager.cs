5e4d029d3e43c4c494bcb5e78b9bf1192c149c50
Saving Progress <97535452+SaveScreen@users.noreply.github.com>
2024-02-15 12:58:39 -0500

    Started working on magic circles

--- Assets/Scripts/WyvernBoss/WyvernBossManager.cs
+++ Assets/Scripts/WyvernBoss/WyvernBossManager.cs
@@ -10,9 +10,17 @@ public class WyvernBossManager : MonoBehaviour
     private GameObject stag;
     private FamiliarScript familiarScript;
     private Rigidbody rb;
+
+    [Header("Fireballs")]
     public GameObject fireball;
     [SerializeField] private float fireballtimer;
+
+    [Header("Magic Circles")]
+    public GameObject magicCircle;
+    [SerializeField] private float spawnradius;
+
     private float startingFireballTimer;
+    [SerializeField] private int phases; //0 = no phase, 1 = fireball, 2 = magic circle, 3 = flamethrower, 4 = flee
 
     // Start is called before the first frame update
     void Start()
@@ -24,7 +32,6 @@ public class WyvernBossManager : MonoBehaviour
         rb = GetComponent<Rigidbody>();
 
         startingFireballTimer = fireballtimer;
-
     }
 
     // Update is called once per frame
@@ -35,20 +42,49 @@ public class WyvernBossManager : MonoBehaviour
         }
         else {
             transform.LookAt(new Vector3(weaver.transform.position.x,transform.position.y,weaver.transform.position.z));
-            if (fireballtimer > 0) {
-                fireballtimer -= Time.deltaTime;
-            }
-            else {
-                if (!playercontroller.isDead) {
-                    ThrowFireball();
-                }
-                fireballtimer = startingFireballTimer;
-            }
         }
+
+        switch (phases) {
+            //FIREBALL
+            case 1:
+                if (!familiarScript.myTurn) {
+                    if (fireballtimer > 0) {
+                        fireballtimer -= Time.deltaTime;
+                    }
+                    else {
+                        if (!playercontroller.isDead) {
+                            ThrowFireball();
+                        }
+                        fireballtimer = startingFireballTimer;
+                    }
+                }
+            break;
+            //MAGIC CIRCLE
+            case 2:
+                if (!familiarScript.myTurn) {
+                    if (fireballtimer > 0) {
+                        fireballtimer -= Time.deltaTime;
+                    }
+                    else {
+                        if (!playercontroller.isDead) {
+                            SpawnMagicCircle();
+                        }
+                        fireballtimer = startingFireballTimer;
+                    }
+                }
+            break;
+        }
+        
     }
 
     void ThrowFireball() {
         Instantiate(fireball,transform.position,Quaternion.identity);
+    }
+
+    void SpawnMagicCircle() {
+        Vector3 randomposition = Random.insideUnitCircle * spawnradius;
+        Vector3 newposition = new Vector3(transform.position.x + randomposition.x,0,transform.position.z + randomposition.y);
+        Instantiate(magicCircle,newposition,Quaternion.identity);
     }
 
     void OnCollisionEnter(Collision other) {
