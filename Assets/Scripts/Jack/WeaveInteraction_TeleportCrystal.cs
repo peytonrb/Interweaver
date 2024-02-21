bbf224bdac8cd532aed8565b29916dd3382a19de
treemanrocket <56743498+treemanrocket@users.noreply.github.com>
2024-02-21 13:57:57 -0500

    teleportation crystal stuff
    teleportation crystal works with the weave rework

--- Assets/Scripts/Jack/WeaveInteraction_TeleportCrystal.cs
+++ Assets/Scripts/Jack/WeaveInteraction_TeleportCrystal.cs
@@ -14,7 +14,7 @@ public class WeaveInteraction_TeleportCrystal : WeaveInteraction
             {
                 if (teleportationCrystalScript.isTeleportable)
                 {
-                    //teleportationCrystalScript.GetOtherCrystals();
+                    teleportationCrystalScript.TeleportFunction();
                     Debug.Log("teleportation interaction called 1");
                 }
             }
@@ -23,7 +23,7 @@ public class WeaveInteraction_TeleportCrystal : WeaveInteraction
             {
                 if (teleportationCrystalScript.isTeleportable)
                 {
-                    //teleportationCrystalScript.GetOtherCrystals();
+                    teleportationCrystalScript.TeleportFunction();
                     Debug.Log("teleportation interaction called 2");
                 }
             }
--- Assets/Scripts/TeleportationCrystalScript.cs
+++ Assets/Scripts/TeleportationCrystalScript.cs
@@ -6,35 +6,20 @@ public class TeleportationCrystalScript : MonoBehaviour
 {
    
     public GameObject linkedCrystal;
-    public bool isTeleportable = true;
+   [HideInInspector] public bool isTeleportable = true;
+    public WeaveController weaveController;
 
-   
-    //public void GetOtherCrystals()
-    //{
-    //    this.GetComponent<WeaveableObject>().ResetWeaveable();
-    //    this.GetComponent<WeaveableObject>().StopAllCoroutines();
-    //    WeaveableManager.Instance.RemoveWeaveableFromList(0, 0);
-    //    Debug.Log("This is the linked crystal's name " + linkedCrystal.name + " and this is it's position " + linkedCrystal.transform.position);
-    //}
-
-    private void OnTriggerEnter (Collider collision)
+    public void TeleportFunction()
     {
-        if (collision.gameObject.CompareTag("Weaveable") && this.GetComponent<WeaveableObject>().hasBeenCombined)
-        {          
-            Debug.Log("this is the weaveable item that it collided with cause physics and shit " + collision.gameObject.name);
-            this.GetComponent<WeaveableObject>().StopAllCoroutines();
-            this.GetComponent<Rigidbody>().useGravity = true;
-            collision.gameObject.GetComponent<WeaveableObject>().StopAllCoroutines();
-            //this.GetComponent<WeaveableObject>().hasBeenCombined = false;
-            collision.gameObject.GetComponent<WeaveableObject>().hasBeenCombined = false;
-            collision.gameObject.GetComponent<WeaveableObject>().ResetWeaveable();
-            
-            
-            collision.transform.position = new Vector3 (linkedCrystal.transform.position.x, 
-                linkedCrystal.transform.position.y, linkedCrystal.transform.position.z + 3);
-        }
-    }
+        this.GetComponent<WeaveableObject>().objectToSnapTo.transform.position = new Vector3(linkedCrystal.transform.position.x,
+                   linkedCrystal.transform.position.y, linkedCrystal.transform.position.z + 3);
 
+        weaveController.currentWeaveable.ResetWeaveable();
+
+        WeaveableManager.Instance.DestroyJoints(weaveController.currentWeaveable.listIndex);
+        
+    }
+     
     void OnDrawGizmos()
     {
         Gizmos.color = Color.blue;
