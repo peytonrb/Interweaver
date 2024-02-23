7355fe1eb371e1dca7b647d61d562075c7f0826c
Saving Progress <97535452+SaveScreen@users.noreply.github.com>
2024-02-23 13:27:04 -0500

    Added glow mushrooms to level manager

--- Assets/Scripts/LevelManager/LevelManagerScript.cs
+++ Assets/Scripts/LevelManager/LevelManagerScript.cs
@@ -7,8 +7,10 @@ public class LevelManagerScript : MonoBehaviour
     public static LevelManagerScript instance;
     public bool cannonSectionIsActive;
     public bool stalactiteSectionIsActive;
+    public bool mushroomSectionIsActive;
     private GameObject[] cannons;
     private GameObject[] stalactites;
+    private GameObject[] glowMushrooms;
     private NetCannonScript netCannonScript;
     private LevelTriggerScript levelTriggerScript;
     public GameObject spikeArea;
@@ -29,6 +31,7 @@ public class LevelManagerScript : MonoBehaviour
     {
         cannons = GameObject.FindGameObjectsWithTag("Cannons");
         stalactites = GameObject.FindGameObjectsWithTag("StalactiteSpawner");
+        glowMushrooms = GameObject.FindGameObjectsWithTag("GlowMushrooms");
 
         if (cannonSectionIsActive) {
             for (int i = 0; i < cannons.Length; i++) {
@@ -45,70 +48,65 @@ public class LevelManagerScript : MonoBehaviour
                 stalactiteScript.canFall = true;
             }
         }
+
+        if (mushroomSectionIsActive) {
+            for (int i = 0; i < glowMushrooms.Length; i++) {
+                TimedGlowMushroomsScript glowMushroomsScript = glowMushrooms[i].GetComponent<TimedGlowMushroomsScript>();
+
+                glowMushroomsScript.isActive = true;
+            }
+        }
         
     }
 
     public void TurnOnOffSection(int section) {
-        levelTriggerScript = GetComponentInChildren<LevelTriggerScript>();
 
         switch (section) {
             //Turn on/off PROJECTILE CANNONS
             case 0:
-                if (levelTriggerScript.triggered) {
-                    for (int i = 0; i < cannons.Length; i++) {
-                        netCannonScript = cannons[i].GetComponent<NetCannonScript>();
+                for (int i = 0; i < cannons.Length; i++) {
+                    netCannonScript = cannons[i].GetComponent<NetCannonScript>();
 
-                        netCannonScript.isOn = true;
-                        Debug.Log("Cannons enabled");
-                    }
-
-                    levelTriggerScript.triggered = false;
-                }
-                else {
-                    for (int i = 0; i < cannons.Length; i++) {
-                        netCannonScript = cannons[i].GetComponent<NetCannonScript>();
-
+                    if (netCannonScript.isOn) {
                         netCannonScript.isOn = false;
-                        Debug.Log("Cannons disabled");
                     }
-
-                    levelTriggerScript.triggered = true;
-                }
+                    else {
+                        netCannonScript.isOn = true;
+                    }
+                    
+                }    
             break;
+
             case 1:
                 if (spikeArea != null) {
                     spikeArea.SetActive(false);
                 }
             break;
+
             case 2:
-                if (levelTriggerScript.triggered) {
-                    for (int i = 0; i < stalactites.Length; i++) {
-                        StalactiteSpawnerScript sss = stalactites[i].GetComponent<StalactiteSpawnerScript>();
+                for (int i = 0; i < stalactites.Length; i++) {
+                    StalactiteSpawnerScript sss = stalactites[i].GetComponent<StalactiteSpawnerScript>();
 
-                        if (sss.canFall) {
-                            sss.canFall = false;
-                        }
-                        else {
-                            sss.canFall = true;
-                        }
+                    if (sss.canFall) {
+                        sss.canFall = false;
                     }
+                    else {
+                        sss.canFall = true;
+                    }
+                }
+            break;
 
-                    levelTriggerScript.triggered = false;
+            case 3:
+            for (int i = 0; i < glowMushrooms.Length; i++) {
+                TimedGlowMushroomsScript mushroomsScript = glowMushrooms[i].GetComponent<TimedGlowMushroomsScript>();
 
+                if (mushroomsScript.isActive) {
+                    mushroomsScript.isActive = false;
                 }
                 else {
-                    for (int i = 0; i < stalactites.Length; i++) {
-                        StalactiteSpawnerScript sss = stalactites[i].GetComponent<StalactiteSpawnerScript>();
-                        
-                        if (sss.canFall) {
-                            sss.canFall = false;
-                        }
-                        else {
-                            sss.canFall = true;
-                        }
-                    }
+                    mushroomsScript.isActive = true;
+                }
 
-                    levelTriggerScript.triggered = true;
                 }
             break;
         }
--- Assets/Scripts/LevelManager/LevelTriggerScript.cs
+++ Assets/Scripts/LevelManager/LevelTriggerScript.cs
@@ -7,7 +7,6 @@ public class LevelTriggerScript : MonoBehaviour
     
     [Tooltip ("0 is for Projectile Cannons, 1 is for Breakeable Spikes, 2 is for stalactites")]
     public int triggerType;
-    [HideInInspector] public bool triggered;
 
     void OnTriggerEnter(Collider other) {
         if (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Familiar")) {
