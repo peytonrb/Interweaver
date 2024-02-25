0820656505f242c923a4bc555becaf3c188a1030
CarolineObvi <carolinejmorian@gmail.com>
2024-02-25 15:59:14 -0500

    fixed issues with mole pillar camera!

--- Assets/Scripts/FamiliarScripts/Mole/MolePillarScript.cs
+++ Assets/Scripts/FamiliarScripts/Mole/MolePillarScript.cs
@@ -13,7 +13,6 @@ public class MolePillarScript : MonoBehaviour
     [SerializeField] private GameObject dirtPillar;
     public List<GameObject> pillarList = new List<GameObject>();
     [SerializeField] private CinemachineVirtualCamera dirtPillarCamera;
-    [SerializeField] private Transform mole;
     private GameObject newPillar;
 
     [Header("Variables")]
@@ -103,27 +102,22 @@ public class MolePillarScript : MonoBehaviour
                 if (!pillarRising) // right as we start things off
                 {
                     movementScript.ZeroCurrentSpeed(); // we do this to prevent sudden jarring movement after movement script is re-enabled
-
                     movementScript.enabled = false; // disable player movement
-
                     pointToRiseTo = new Vector3 (pillarToRaise.transform.position.x, transform.position.y, 
-                        pillarToRaise.transform.position.z) + (Vector3.up * maxPillarHeight); // set a destination for the pillar to rise
-
+                    pillarToRaise.transform.position.z) + (Vector3.up * maxPillarHeight); // set a destination for the pillar to rise
                     pillarRising = true; // mark that pillar has started rising
+                    dirtPillarCamera.Follow = pillarToRaise.transform; // set camera to pillar top
                 }
                 else if (distance > 0.1f && pillarRising) // if pillar hasn't quite reached destination, and we're still meant to be rising
                 {
                     pillarToRaise.transform.position = Vector3.MoveTowards(pillarToRaise.transform.position, 
-                        pointToRiseTo, pillarBuildSpeed * Time.deltaTime);
+                    pointToRiseTo, pillarBuildSpeed * Time.deltaTime);
                 }
                 else // once pillar has reached its destination or we've decided it needs to stop rising through other means
                 {
                     PillarRiseEnd();
                 }
             }
-
-            dirtPillarCamera.Follow = newPillar.transform.GetChild(1);
-            
         }
     }
 
@@ -142,23 +136,21 @@ public class MolePillarScript : MonoBehaviour
                 if (pillarToLower.transform.parent != null)
                 {
                     pillarToLower = pillarToLower.transform.parent.gameObject; // if we have a top, get it
-                    dirtPillarCamera.Follow = pillarToLower.transform.GetChild (1);
                 }
                 if (!pillarLowering) // right as we start things off
                 {
                     movementScript.ZeroCurrentSpeed(); // we do this to prevent sudden jarring movement after movement script is re-enabled
                     movementScript.enabled = false; // disable player movement
                     pillarLowering = true;
+                    dirtPillarCamera.Follow = pillarToLower.transform; // set camera to pillar top
                 }
                 pillarToLower.transform.position = Vector3.MoveTowards(pillarToLower.transform.position, 
-                    pillarToLower.transform.position - transform.up, pillarBuildSpeed * Time.deltaTime);
+                pillarToLower.transform.position - transform.up, pillarBuildSpeed * Time.deltaTime);
             }
             else
             {
                 PillarLowerEnd();
             }
-
-            dirtPillarCamera.Follow = newPillar.transform.GetChild(1);
         }
     }
 
@@ -170,7 +162,7 @@ public class MolePillarScript : MonoBehaviour
         }
         rise = false;
         pillarRising = false;
-        dirtPillarCamera.Follow = mole;
+        dirtPillarCamera.Follow = transform;
     }
 
     public void PillarLowerEnd()
@@ -181,7 +173,7 @@ public class MolePillarScript : MonoBehaviour
         }
         lower = false;
         pillarLowering = false;
-        dirtPillarCamera.Follow = mole;
+        dirtPillarCamera.Follow = transform;
     }
 
     public GameObject SearchForNearbyPillars()
