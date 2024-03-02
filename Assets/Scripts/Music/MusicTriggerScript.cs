ce01876eb66bae6ba7fc7328693faa4dc96f3ded
CarolineObvi <carolinejmorian@gmail.com>
2024-03-01 23:25:11 -0500

    dynamic music v2

--- Assets/Scripts/Music/MusicTriggerScript.cs
+++ Assets/Scripts/Music/MusicTriggerScript.cs
@@ -13,35 +13,78 @@ public class MusicTriggerScript : MonoBehaviour
     [Header("Generic Variables")]
     [SerializeField] AudioClip musicToTransitionTo;
     [SerializeField] bool active = true;
-    private AudioClip previousMusic; // save the music that was removed if we wish to go back to it at any point
+    private float songTime;
     [Header("Transition Variables")]
     [SerializeField][Range (0.1f, 10f)] float fadeOutTransitionDuration = 2f; 
     [SerializeField][Range (0.1f, 10f)] float fadeInTransitionDuration = 5f;
     [Header("Add Layer Variables")]
     [SerializeField] private List<AudioClip> layersToAdd = new List<AudioClip>();
 
+    public bool tripped;
+    public List<GameObject> currentCharacters = new List<GameObject>();
+
     void Start()
     {
         audioManager = GameObject.FindGameObjectWithTag("Audio Manager").GetComponent<AudioManager>();
     }
-    
-    private void OnTriggerEnter(Collider collider)
-    { 
-        if ((collider.CompareTag("Player") || collider.CompareTag("Familiar")) && active)
+
+    void Update()
+    {
+        if (tripped)
         {
-            active = false;
-            if (clearPreviousLayers)
+            songTime = audioManager.musicChannel.time;
+            Debug.Log(songTime);
+            if (!gameObject.GetComponent<Collider>().bounds.Contains(currentCharacters[0].transform.position))
             {
-                audioManager.ClearMusicLayers();
+                tripped = false;
+                currentCharacters.Remove(currentCharacters[0]);
             }
-            if (transitionMode)
+        }
+    }
+
+    private void OnTriggerStay(Collider collider)
+    {
+        if (!tripped) // if a trigger is newly entered
+        {
+            if (gameObject.GetComponent<Collider>().bounds.Contains(collider.transform.position))
             {
-                previousMusic = audioManager.musicChannel.clip;
-                StartCoroutine(audioManager.StartMusicFadeOut(musicToTransitionTo, fadeOutTransitionDuration, fadeInTransitionDuration));
+                MovementScript colliderMovementScript = collider.gameObject.GetComponent<MovementScript>();
+
+                if (colliderMovementScript.active && !currentCharacters.Contains(collider.gameObject) && !audioManager.transitioning)
+                {
+                    ChangeTrack();
+                    tripped = true;
+                    currentCharacters.Add(collider.gameObject);
+                }
             }
-            if (addLayerMode)
+        }
+        else
+        {
+            MovementScript colliderMovementScript = collider.gameObject.GetComponent<MovementScript>();
+
+            if (!colliderMovementScript.active)
             {
-                audioManager.AddMusicLayer(layersToAdd[0], true);
+                tripped = false;
+                currentCharacters.Remove(collider.gameObject);
+            }
+        }
+    }
+
+    private void ChangeTrack()
+    {
+        if (clearPreviousLayers)
+        {
+            audioManager.ClearMusicLayers();
+        }
+        if (transitionMode)
+        {
+            StartCoroutine(audioManager.StartMusicFadeOut(musicToTransitionTo, fadeOutTransitionDuration, fadeInTransitionDuration, songTime));
+        }
+        if (addLayerMode)
+        {
+            foreach (AudioClip layerToAdd in layersToAdd)
+            {
+                audioManager.AddMusicLayer(layerToAdd, true);
             }
         }
     }
