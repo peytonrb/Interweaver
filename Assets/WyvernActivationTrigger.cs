d672cef251588d89e9d0286c373b7d9992f64142
Saving Progress <97535452+SaveScreen@users.noreply.github.com>
2024-03-03 14:59:18 -0500

    Wyvern activation trigger made

---
+++ Assets/WyvernActivationTrigger.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WyvernActivationTrigger : MonoBehaviour
+{
+    [SerializeField] private GameObject wyvern;
+    [SerializeField] private bool destroyThisAfterActivation;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+       wyvern.SetActive(false); 
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Player")) {
+            ActivateWyvern();
+        }
+    }
+
+    public void ActivateWyvern() {
+        wyvern.SetActive(true);
+
+        if (destroyThisAfterActivation) {
+            Destroy(gameObject);
+        }
+        else {
+            gameObject.SetActive(false);
+        }
+        
+    }
+
+    public void ResetTrigger() {
+        gameObject.SetActive(true);
+        wyvern.SetActive(false);
+    }
+}
