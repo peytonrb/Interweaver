1d8b949efad7d0c2684ca2ca8ac9ef8fb1e16a56
treemanrocket <56743498+treemanrocket@users.noreply.github.com>
2024-03-14 19:23:20 -0400

    buttons should be aligned
    aligned buttons for UI stuff

--- Assets/Scripts/PopUpUi Script/PopUIScript.cs
+++ Assets/Scripts/PopUpUi Script/PopUIScript.cs
@@ -7,12 +7,22 @@ public class PopUIScript : MonoBehaviour
 
     [SerializeField] private Camera mainCamera;
     [SerializeField] private Transform targetTransform;
+    [SerializeField] private bool isSpeakerPost;
     private Vector3 offset = new Vector3(0, 3.5f, 0);
 
     // Update is called once per frame
+    private void Start()
+    {
+        isSpeakerPost = false;
+    }
     void Update()
     {
         transform.rotation = mainCamera.transform.rotation;
-        transform.position = targetTransform.position + offset;
+
+        if(!isSpeakerPost) 
+        {
+            transform.position = targetTransform.position + offset;
+        }
+        
     }
 }
