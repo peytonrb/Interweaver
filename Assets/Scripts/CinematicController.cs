68c52b36b6c8c524646e0ba8034b5abeb06d72fa
Peyton Bischof <peyton.bischof@hotmail.com>
2024-04-12 17:48:48 -0400

    cinematic recordings

--- Assets/Scripts/CinematicController.cs
+++ Assets/Scripts/CinematicController.cs
@@ -6,22 +6,10 @@ using UnityEngine.VFX;
 public class CinematicController : MonoBehaviour
 {
     [Header("Clip 1")]
-    public VisualEffect deathVFX;
     public GameObject explosionPS;
 
     public void Clip1()
     {
-        deathVFX.gameObject.SetActive(true);
-        deathVFX.Play();
-        StartCoroutine(Wait());
-    }
-
-    IEnumerator Wait()
-    {
-        yield return new WaitForSeconds(6.5f);
-        deathVFX.gameObject.SetActive(false);
-        explosionPS.SetActive(true);
-
         foreach (ParticleSystem ps in explosionPS.GetComponentsInChildren<ParticleSystem>())
         {
             ps.Play();
