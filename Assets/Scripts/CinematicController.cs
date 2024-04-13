b983589f9bd85d22b711372220fe13cd671e2683
Peyton Bischof <peyton.bischof@hotmail.com>
2024-04-13 10:58:54 -0400

    cinematic progress

--- Assets/Scripts/CinematicController.cs
+++ Assets/Scripts/CinematicController.cs
@@ -8,11 +8,33 @@ public class CinematicController : MonoBehaviour
     [Header("Clip 1")]
     public GameObject explosionPS;
 
+    [Header("Clip 2")]
+    public GameObject transformPS;
+    public ParticleSystem flashPS;
+    public GameObject rival;
+
     public void Clip1()
     {
         foreach (ParticleSystem ps in explosionPS.GetComponentsInChildren<ParticleSystem>())
         {
             ps.Play();
         }
+    }
+
+    public void PlayTransformEffect()
+    {
+        transformPS.SetActive(true);
+    }
+
+    public void StopTransformEffect()
+    {
+        transformPS.SetActive(false);
+    }
+
+    public void PlayFlash()
+    {
+        flashPS.gameObject.SetActive(true);
+        rival.SetActive(false);
+        flashPS.Play();
     }
 }
