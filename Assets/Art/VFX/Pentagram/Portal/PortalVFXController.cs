9010b2a0c558491d48cc7a32dfd257155d48d049
Peyton Bischof <peyton.bischof@hotmail.com>
2024-04-14 12:04:40 -0400

    cinematics and vfx

--- Assets/Art/VFX/Pentagram/Portal/PortalVFXController.cs
+++ Assets/Art/VFX/Pentagram/Portal/PortalVFXController.cs
@@ -3,25 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.VFX;
 
+// dear anyone who may come upon this script - i greatly apologize
+
 public class PortalVFXController : MonoBehaviour
 {
     public bool isActive;
     private bool isPlaying = false;
     public GameObject orb;
     public GameObject[] effects;
+    public GameObject[] startingPS;
+    public GameObject[] endingPS;
 
     void Update()
     {
         if (isActive && !isPlaying)
         {
             isPlaying = true;
-
-            foreach (GameObject effect in effects)
+            foreach (GameObject ps in startingPS)
             {
-                effect.SetActive(true);
+                ps.gameObject.SetActive(true);
+                ps.GetComponent<ParticleSystem>().Play();
+                StartCoroutine(ScalePS(startingPS[3]));
+                StartCoroutine(More());
             }
-
-            StartCoroutine(Wait());
         }
 
         if (!isActive && isPlaying)
@@ -33,6 +37,31 @@ public class PortalVFXController : MonoBehaviour
 
             isPlaying = false;
         }
+    }
+
+    IEnumerator ScalePS(GameObject ps)
+    {
+        yield return new WaitForSeconds(0.5f);
+        float t = 0;
+        ParticleSystem.ShapeModule shapePS = ps.GetComponent<ParticleSystem>().shape;
+        float startRadius = shapePS.radius;
+        while (t < 1f)
+        {
+            t += Time.deltaTime;
+            shapePS.radius = Mathf.Lerp(startRadius, 9f, t);
+            yield return null;
+        }
+    }
+
+    IEnumerator More()
+    {
+        yield return new WaitForSeconds(0.75f);
+        foreach (GameObject effect in effects)
+        {
+            effect.SetActive(true);
+        }
+
+        StartCoroutine(Wait());
     }
 
     IEnumerator Wait()
@@ -63,13 +92,46 @@ public class PortalVFXController : MonoBehaviour
         float t = 0;
         Material material = orb.GetComponent<Renderer>().material;
         float startOpacity = material.GetFloat("_Alpha");
+        bool hasPlayed = false;
 
         while (t < 1f)
         {
             t += Time.deltaTime;
+
+            if (t < 0.75f && !hasPlayed)
+            {
+                hasPlayed = true;
+                StartCoroutine(idec());
+
+                foreach (GameObject ps in endingPS)
+                {
+                    ps.SetActive(true);
+                    ps.GetComponent<ParticleSystem>().Play();
+                    StartCoroutine(ScalePS(endingPS[5]));
+                }
+            }
+
             float blend = Mathf.Clamp01(t / 1f);
             material.SetFloat("_Alpha", Mathf.Lerp(startOpacity, 0f, blend));
             yield return null;
         }
+
+        StartCoroutine(WaitForDeath());
+    }
+
+    IEnumerator idec()
+    {
+        yield return new WaitForSeconds(0.25f);
+        foreach (GameObject effect in effects)
+        {
+            if (effect.name != "Orb")
+                effect.SetActive(false);
+        }
+    }
+
+    IEnumerator WaitForDeath()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Destroy(gameObject);
     }
 }
