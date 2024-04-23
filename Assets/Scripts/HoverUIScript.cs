555f9915e96ed8016bd1cc0fa0c8ad4c8e9ed42e
Saving Progress <97535452+SaveScreen@users.noreply.github.com>
2024-04-23 14:55:34 -0400

    fix for blackboard UI selection

--- Assets/Scripts/HoverUIScript.cs
+++ Assets/Scripts/HoverUIScript.cs
@@ -2,16 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class HoverUIScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class HoverUIScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, ISelectHandler, IDeselectHandler
 {
     public GameObject myLostSoulCount;
+    private CanvasGroup canvasGroup;
+
+    void Start() 
+    {
+        canvasGroup = gameObject.GetComponent<CanvasGroup>();
+
+        canvasGroup.alpha = 0;
+    }
+
+    public void OnSelect(BaseEventData data) {
+        canvasGroup.alpha = 1;
+    }
+
+    public void OnDeselect(BaseEventData data) {
+        canvasGroup.alpha = 0;
+    }
 
     public void OnPointerEnter(PointerEventData eventData) {
         myLostSoulCount.SetActive(true);
+        canvasGroup.alpha = 1;
     }
 
     public void OnPointerExit(PointerEventData eventData) {
         myLostSoulCount.SetActive(false);
+        canvasGroup.alpha = 0;
     }
 }
